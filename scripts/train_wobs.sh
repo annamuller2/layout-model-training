aa2606273945db4eefbd84bd6bf665b1ec8ae2f1
amuller2@nd.edu <annaemuller@DESKTOP-5QN1S86>
2023-03-01 20:32:54 -0500

    update script

--- scripts/train_wobs.sh
+++ scripts/train_wobs.sh
@@ -2,16 +2,12 @@
 
 cd ../tools
 
-python convert_prima_to_coco.py \
-    --prima_datapath ../data/wob \
-    --anno_savepath ../data/wob/annotations.json 
-
-python train_net.py \
-    --dataset_name          wob-layout \
-    --json_annotation_train ../data/wob/annotations-train.json \
-    --image_path_train      ../data/wob/Images \
-    --json_annotation_val   ../data/wob/annotations-val.json \
-    --image_path_val        ../data/wob/Images \
-    --config-file           ../configs/wob/mask_rcnn_R_50_FPN_3x.yaml \
+python3 train_net.py \
+    --dataset_name          wob-item \
+    --json_annotation_train ../data/wob/train.json \
+    --image_path_train      ../data/wob/ \
+    --json_annotation_val   ../data/wob/test.json \
+    --image_path_val        ../data/wob/ \
+    --config-file           ../configs/prima/mask_rcnn_R_50_FPN_3x.yaml \
     OUTPUT_DIR  ../outputs/wob/mask_rcnn_R_50_FPN_3x/ \
     SOLVER.IMS_PER_BATCH 2 
